6465e448b864a765a7767e4d304e9bdbcde689c1
Justin Melendez <104957885+CountDown2Extinction@users.noreply.github.com>
2022-06-23 17:40:24 -0500

    checker board part 1

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// Your JS goes here
+
+var container = document.createElement('div');
+let counter = 0
+document.body.append(container);
+console.log('hello')
+for (var i = 0; i < 81; i++) {
+  
+    var div = document.createElement('div');
+    counter ++;
+    div.style.width = '10%'
+    div.style.float = 'left'
+    div.style.paddingBottom = '10%'
+    div.style.border = '1px solid black'
+    div.style.background = (counter %2 ===0 ?
+        'red': 'black')
+    
+
+    container.append(div);
+ console.log(typeof(div));
+}
+
+
+    
+console.log(typeof(container));
